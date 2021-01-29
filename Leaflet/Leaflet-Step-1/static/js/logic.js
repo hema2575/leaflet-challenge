f52266eb49753b2849f8d9f1ba3d2ba24499b9aa
chilli <70721729+hema2575@users.noreply.github.com>
2021-01-28 22:11:40 -0600

    updated the code to render the map

--- Leaflet/Leaflet-Step-1/static/js/logic.js
+++ Leaflet/Leaflet-Step-1/static/js/logic.js
@@ -9,10 +9,10 @@ var thisMap = L.map("map", {
   
   // Adding a tile layer (the background map image) to our map
   // We use the addTo method to add objects to our map
-  L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
-    id: "mapbox.streets",
+    id: "mapbox/streets-v11",
     accessToken: "pk.eyJ1IjoiYXJ0ZXhwbyIsImEiOiJja2toZW9qY2IwNGtuMnBtcjc3dXlhbzl0In0.eRrDXMW4viTogHAFUnkFxw"
   }).addTo(thisMap);
 
